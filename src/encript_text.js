5efbf9442d200ea4f14bea4b7d37507b8436ef25
Thiago Rodrigues <thiago2003.trs@gmail.com>
2022-05-29 16:13:49 -0300

    Add script do encriptador/desencriptador de texto

---
+++ src/encript_text.js
@@ -0,0 +1,92 @@
+const splitParameter1 = /(a|e|i|o|u)/g;
+const splitParameter2 = /(ai|enter|imes|ober|ufat)/g;
+const blockParameter = /!|#|@|%|¨|&|\*|\(|\)|-|\+|=|`|'|"|'|\{|\}|\||\\|;|\/|_|\[|\]|\^|\~|<|>|:|\$|¬|─|\d/g;
+
+function replaceArray(array, type) {
+    let newArray = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (type === "encryption") {
+            switch (array[i]) {
+                case "a":
+                    array[i] = array[i].replace("a", "ai");
+                    break;
+
+                case "e":
+                    array[i] = array[i].replace("e", "enter");
+                    break;
+
+                case "i":
+                    array[i] = array[i].replace("i", "imes");
+                    break;
+
+                case "o":
+                    array[i] = array[i].replace("o", "ober");
+                    break;
+
+                case "u":
+                    array[i] = array[i].replace("u", "ufat");
+                    break;
+
+                default:
+                    array[i] = array[i];
+            }
+
+        } else if (type === "decryption") {
+            switch (array[i]) {
+                case "ai":
+                    array[i] = array[i].replace("ai", "a");
+                    break;
+
+                case "enter":
+                    array[i] = array[i].replace("enter", "e");
+                    break;
+
+                case "imes":
+                    array[i] = array[i].replace("imes", "i");
+                    break;
+
+                case "ober":
+                    array[i] = array[i].replace("ober", "o");
+                    break;
+
+                case "ufat":
+                    array[i] = array[i].replace("ufat", "u");
+                    break;
+
+                default:
+                    array[i] = array[i];
+            }
+        }
+
+        newArray.push(array[i]);
+    }
+
+    return newArray;
+}
+
+
+function encryptText() {
+    userOutput.value = "";
+
+    let inputText = getValue(userInput);
+    let textArray = splitText(inputText, splitParameter1);
+    let replacedArray = replaceArray(textArray, "encryption");
+    let outputText = concatArray(replacedArray);
+    checkText(userOutput, outputText);
+    clearInput(userInput);
+}
+
+function decryptText() {
+    userOutput.value = "";
+
+    let inputText = getValue(userInput);
+    let textArray = splitText(inputText, splitParameter2);
+    let replacedArray = replaceArray(textArray, "decryption");
+    let outputText = concatArray(replacedArray);
+    checkText(userOutput, outputText);
+    clearInput(userInput);
+}
+
+encryptBtn.addEventListener("click", encryptText);
+decryptBtn.addEventListener("click", decryptText);
